96f4cb56bba571da15687c6e05c1931a9e46be39
fozlaa <fr124438@gmail.com>
2026-09-16 02:29:20 +0600

    Initial project setup

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import xgboost
+
+print("CS2 Market Model")
+print("----------------")
+print("Python is working!")
+print("Pandas:", pd.__version__)
+print("NumPy:", np.__version__)
+print("Scikit-learn:", sklearn.__version__)
+print("XGBoost:", xgboost.__version__)
